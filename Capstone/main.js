2c67ed69577c327d7ce8003ef7f503a738bfc3ca
Prerna <prerna1998.ps@gmail.com>
2020-05-07 20:13:29 +0530

    Components

--- Capstone/main.js
+++ Capstone/main.js
@@ -1,27 +1,71 @@
-var app=new Vue({
-    el:'#app',
-    data:{
-        brand:"Pi",
-        product:"Umbrella",
-        // image:"purple.jpg",
-        selectedVariant:0,
-        // inStock:true,
-        details:["A1 Fabric","Gender Neutral", "Long Lasting"],
-        variants:[
-            {
-                variantColor :"purple",
-                variantId:234,
-                variantImage:"purple.jpg",
-                variantQuantity:20
-            },
-            {
-                variantColor :"yellow",
-                variantId:259,
-                variantImage:"yellow.png",
-                variantQuantity:0
-            }
-        ],
-        cart:0
+Vue.component('product',{
+    props:{
+        premium:{
+            type:Boolean,
+            required:true
+        }
+    },
+    template:`
+    <div class ="product">
+        <div>
+            <img class ="product-image" v-bind:src="image" />
+        </div>
+    
+        <div class ="product-info">
+            <h3>{{title}}</h3>
+            <h4 v-if="inStock">In Stock</h4>
+            <h4 v-else="inStock">Out of Stock</h4>
+            <p>User is premium account holder : {{premium}}</p>
+            <p>Shipping : {{shipping}}</p>
+
+        </div>
+    
+        <ul>
+            <li v-for="detail in details">{{detail}}</li>
+        </ul>
+        
+        <div id="color-selector">
+            <div v-for="(variant,index) in variants" 
+            v-bind:key="variant.variantId"
+            v-bind:style="{backgroundColor:variant.variantColor}"
+            class="color-box"
+            v-on:mouseover="updateProduct(index)">
+        </div>
+    
+        <button v-on:click="addToCart" 
+                v-bind:disabled="!inStock"
+                v-bind:class="{disabledButton:!inStock}">
+                Add To Cart
+        </button>
+    
+        <div>
+            <p>Cart({{cart}})</p>
+        </div>
+    </div>`,
+    data:function(){
+        return{
+            brand:"Pi",
+            product:"Umbrella",
+            // image:"purple.jpg",
+            selectedVariant:0,
+            // inStock:true,
+            details:["A1 Fabric","Gender Neutral", "Long Lasting"],
+            variants:[
+                {
+                    variantColor :"purple",
+                    variantId:234,
+                    variantImage:"purple.jpg",
+                    variantQuantity:20
+                },
+                {
+                    variantColor :"yellow",
+                    variantId:259,
+                    variantImage:"yellow.png",
+                    variantQuantity:0
+                }
+            ],
+            cart:0
+        }  
     },
     methods:{
         addToCart:function(){
@@ -41,5 +85,20 @@ var app=new Vue({
         inStock:function(){
             return this.variants[this.selectedVariant].variantQuantity
         },
+        shipping:function(){
+            if(this.premium)
+                return "Free shipping"
+            else
+                return "167 INR"
+        },
+    }
+})
+
+
+
+var app=new Vue({
+    el:'#app',
+    data:{
+        premium:true
     }
 })
