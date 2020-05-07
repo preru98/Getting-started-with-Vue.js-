3d23f7ef6ea3ef26cabc95251fb7c0c81dbe5c3a
Prerna <prerna1998.ps@gmail.com>
2020-05-07 16:12:43 +0530

    Coputed Properties

--- Capstone/main.js
+++ Capstone/main.js
@@ -1,20 +1,24 @@
 var app=new Vue({
     el:'#app',
     data:{
+        brand:"Pi",
         product:"Umbrella",
-        image:"purple.jpg",
-        inStock:true,
+        // image:"purple.jpg",
+        selectedVariant:0,
+        // inStock:true,
         details:["A1 Fabric","Gender Neutral", "Long Lasting"],
         variants:[
             {
                 variantColor :"purple",
                 variantId:234,
-                variantImage:"purple.jpg"
+                variantImage:"purple.jpg",
+                variantQuantity:20
             },
             {
                 variantColor :"yellow",
                 variantId:259,
-                variantImage:"yellow.png"
+                variantImage:"yellow.png",
+                variantQuantity:0
             }
         ],
         cart:0
@@ -23,8 +27,20 @@ var app=new Vue({
         addToCart:function(){
             this.cart+=1
         },
-        updateProduct: function(variantImage){
-            this.image=variantImage
+        updateProduct: function(index){
+            this.selectedVariant=index
         }  
+    },
+    computed:{
+        title:function(){
+            return this.brand+' '+this.product
+        },
+        image:function(){
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock:function(){
+            return this.variants[this.selectedVariant].variantQuantity
+        },
     }
+
 })
